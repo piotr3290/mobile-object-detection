8797757b607b3694a13dcab043fb67830b5bbb17
Piotr Wroblewski <piotr3290@wp.pl>
2023-04-13 22:59:47 +0200

    ui implementation

--- app/src/main/java/com/example/objectdetection/MainActivity.kt
+++ app/src/main/java/com/example/objectdetection/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
@@ -17,11 +17,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val module: Module = LiteModuleLoader.loadModuleFromAsset(assets, "model.pt")
-
         setContent {
             ObjectDetectionTheme {
 
+                var moduleName by remember {
+                    mutableStateOf("BasicModel")
+                }
+                val module: Module = LiteModuleLoader.loadModuleFromAsset(assets, "basicModel.pt")
                 val navController = rememberNavController()
 
                 Scaffold(topBar = {
@@ -30,7 +32,8 @@ class MainActivity : ComponentActivity() {
                     Navigation(
                         navController = navController,
                         modifier = Modifier.padding(contentPadding),
-                        module = module
+                        module = module,
+                        modelName = moduleName
                     )
                 }
 
--- app/src/main/java/com/example/objectdetection/MainScreen.kt
+++ app/src/main/java/com/example/objectdetection/MainScreen.kt
@@ -1,29 +1,90 @@
 package com.example.objectdetection
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
-fun MainScreen(navController: NavHostController) {
-    LazyColumn {
-        item {
-            Button(onClick = {
-                navController.navigate(Screen.ImageScreen.route)
-            }) {
-                Text(text = "IMAGE")
+fun MainScreen(navController: NavHostController, modelName: String) {
+    Scaffold(bottomBar = {
+        Text(
+            text = modelName,
+            color = Color.Black,
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            textAlign = TextAlign.Center
+        )
+    }) { contentPadding ->
+        LazyColumn(
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
+        ) {
+            item {
+                FloatingActionButton(
+                    shape = MaterialTheme.shapes.large,
+                    onClick = {
+                        navController.navigate(Screen.ImageScreen.route)
+                    },
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.photo_camera_48),
+                        contentDescription = "Photo"
+                    )
+                }
             }
-        }
 
-        item {
-            Button(onClick = {
-                navController.navigate(Screen.CameraScreen.route)
-            }) {
-                Text(text = "CAMERA")
+            item {
+                FloatingActionButton(
+                    shape = MaterialTheme.shapes.large,
+                    onClick = {
+                        navController.navigate(Screen.CameraScreen.route)
+                    },
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_photo_camera_front_48),
+                        contentDescription = "Camera"
+                    )
+                }
+            }
+
+            item {
+                FloatingActionButton(
+                    shape = MaterialTheme.shapes.large,
+                    onClick = {
+                        //TODO - implement loading model
+                    },
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_file_upload_48),
+                        contentDescription = "Model"
+                    )
+                }
             }
         }
     }
+
 
 }
