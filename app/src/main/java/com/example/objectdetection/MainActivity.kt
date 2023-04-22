457c7ed3bb20abcc0aba432619d0a6c9b199bac1
DominikBuj <dominik.bujnowicz2@gmail.com>
2023-04-22 12:32:12 +0200

    using material design 3 theme

--- app/src/main/java/com/example/objectdetection/MainActivity.kt
+++ app/src/main/java/com/example/objectdetection/MainActivity.kt
@@ -9,16 +9,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.example.objectdetection.ui.theme.ObjectDetectionTheme
+import com.example.objectdetection.ui.theme.AppTheme
 import org.pytorch.LiteModuleLoader
 import org.pytorch.Module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            ObjectDetectionTheme {
+            AppTheme {
 
                 val moduleName by remember {
                     mutableStateOf("YOLO v5")
@@ -46,7 +45,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ObjectDetectionTheme {
-    }
-
+    AppTheme {}
 }
